a564b0bf089f2310558af44e2f4a6e48664cbde2
Rajesh Ravindran <rajesh.ravindran@thoughtspot.com>
2018-12-03 12:52:41 -0800

    updated the .sh file

--- runTSUtility.sh
+++ runTSUtility.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-. tsUtility_config.ini
+source: tsUtility_config.ini
 source ${venv}/activate
 
 echo "Python env activated"
 
-echo ${tsadmin}
-echo ${tspassword}
+python -m user_mgmt.ts_ug_util -h
 
 deactivate
 
