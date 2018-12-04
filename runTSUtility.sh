fed8420af037d1107f508d5d62a83053af8314dc
Rajesh Ravindran <rajesh.ravindran@thoughtspot.com>
2018-12-04 09:32:07 -0800

    updated unix shell

--- runTSUtility.sh
+++ runTSUtility.sh
@@ -3,7 +3,22 @@
 source: tsUtility_config.ini
 
 
+echo "STARTING TS UTILITY BASH"
 
+
+case ${type} in
+    delete)
+            python -m user_mgmt.ts_ug_util --ts_url ${url} --username ${username} --password {}
+        ;;
+
+    get)
+
+        ;;
+
+    sync_excel )
+
+        ;;
+esac
 
 
 source ${venv}/activate
