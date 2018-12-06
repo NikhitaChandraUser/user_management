5c53efc9967000c21671fdaf320668799fbae07e
NikhitaChandraUser <nikhita.chandra@thoughtspot.com>
2018-12-06 11:19:58 -0800

    Merge branch 'master' of https://github.com/NikhitaChandraUser/user_management
    removed bash files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='user_management',
       description='This package contains all the user management functionality in ThoughtSpot',
       url='/Users/nikhita.chandra/Desktop/Cetera2',
       # Need to fix url - Git URL
-      author='Nikhita Chadra',
+      author='Nikhita Chandra',
       author_email='nikhita.chandra@thoughtspot.com',
       license='MIT',
       packages=find_packages(),
